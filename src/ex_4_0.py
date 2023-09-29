61e3ecc7e601f8168b7935369c0fce6f947a7815
mesa96 <143638933+mesa96@users.noreply.github.com>
2023-09-29 07:22:25 -0600

    Completed 4_0

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -10,10 +10,13 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def get_shutdown_events(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation
-    """
-    pass
+    shutdown_events = []  # Initialize an empty list to store shutdown events
+    with open(logfile, 'r') as file:
+        for line in file:
+            if "Shutdown initiated" in line:  # Check if the line contains "Shutdown initiated"
+                shutdown_events.append(line.strip())  # Add the line to the list
+    
+    return shutdown_events  # Return the list of shutdown events
 
 
 # >>>> The code below will call your function and print the results
