d731d061a9ba5a3db1416bf6609b3065deb8a633
mesa96 <143638933+mesa96@users.noreply.github.com>
2023-09-29 07:24:42 -0600

    Completed 4_1

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -14,10 +14,8 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def num_shutdowns(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
+    shutdown_events = get_shutdown_events(logfile)  # Use the function from ex_4_0 to get shutdown events
+    return len(shutdown_events)  # Return the number of shutdown events
 
 
 # >>>> The code below will call your function and print the results
